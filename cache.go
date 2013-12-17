d73f587305c28b8c96bd0cd788f15ba9376d23b4
Vincze, Adam <adam.vincze@asideas.de>
2013-12-17 11:23:43 +0100

    locking error fixed

--- cache.go
+++ cache.go
@@ -25,44 +25,67 @@ func init() {
 }
 
 func (c *cache) put(dur time.Duration, key string, obj interface{}) {
-	log.Printf("[%s] caching", key)
+	log.Printf("[%s] caching put", key)
+	log.Printf("[%s] caching acquiring write lock", key)
 	c.Lock()
+	defer c.Unlock()
+
 	c.cacheMap[key] = &cachedObj{
 		expire: time.Now().Add(dur),
 		obj:    obj,
 	}
-	c.Unlock()
-	log.Printf("[%s] caching finished", key)
+
+	log.Printf("[%s] caching releasing write lock", key)
 }
 
-func (c *cache) get(key string) interface{} {
-	now := time.Now()
-	log.Printf("looking for [%s] in the cache", key)
+func (c *cache) get(key string) (*cachedObj, bool) {
+	log.Printf("[%s] caching get", key)
+	log.Printf("[%s] caching acquiring read lock", key)
 	c.RLock()
-	if cachedObj, ok := c.cacheMap[key]; ok {
-		c.RUnlock()
-		log.Printf("found [%s] in the cache", key)
+	defer c.RUnlock()
+
+	cachedObj, ok := c.cacheMap[key]
+	log.Printf("[%s] caching releasing read lock", key)
+	return cachedObj, ok
+}
+
+func (c *cache) getOrExpire(key string) interface{} {
+	log.Printf("[%s] caching getOrExpire", key)
+	now := time.Now()
+	cachedObj, ok := c.get(key)
+	if ok {
+		log.Printf("[%s] found in cache", key)
 		if cachedObj.expire.After(now) {
+			log.Printf("[%s] cache returning ", key)
 			return cachedObj.obj
 		} else {
 			log.Printf("[%s] cache expired", key)
-			c.Lock()
-			delete(c.cacheMap, key)
-			c.Unlock()
+			c.del(key)
 		}
 	}
-	c.RUnlock()
 	return nil
 }
 
+func (c *cache) del(key string) {
+	log.Printf("[%s] caching del", key)
+	log.Printf("[%s] caching acquiring write(del) lock", key)
+	c.Lock()
+	defer c.Unlock()
+
+	delete(c.cacheMap, key)
+	log.Printf("[%s] caching releasing write(del) lock", key)
+}
+
 func (c *cache) getOrRunCacheAndReturn(dur time.Duration, key string, retrieveFn func() interface{}) interface{} {
-	if obj := c.get(key); obj != nil {
-		log.Printf("[%s] returning cached instance", key)
+	log.Printf("[%s] caching getOrRunCacheAndReturn", key)
+	if obj := c.getOrExpire(key); obj != nil {
+		log.Printf("[%s] getOrRunCacheAndReturn returning cached instance", key)
 		return obj
 	}
-
+	log.Printf("[%s] getOrRunCacheAndReturn retrieving instance from jira", key)
 	obj := retrieveFn()
+	log.Printf("[%s] getOrRunCacheAndReturn caching retrieved instance", key)
 	c.put(dur, key, obj)
-	log.Printf("[%s] caching done returning", key)
+	log.Printf("[%s] getOrRunCacheAndReturn caching done returning", key)
 	return obj
 }
